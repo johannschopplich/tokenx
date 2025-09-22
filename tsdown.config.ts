28563ca1c6cc0b4b3400c4f8dfe42ebefe9a9619
Johann Schopplich <mail@johannschopplich.com>
2025-09-22 16:19:37 +0200

    perf: optimize tsdown build size

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'tsdown/config'
 const config: UserConfig | UserConfigFn = defineConfig({
   entry: ['src/index.ts'],
   dts: true,
-  unbundle: true,
 })
 
 export default config
