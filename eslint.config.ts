7fa507e47844ddea93ce3acbcfaa2133267e403c
Johann Schopplich <mail@johannschopplich.com>
2026-03-06 08:14:36 +0100

    chore: update ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,7 @@
+import type { ConfigNames, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+const config: FlatConfigComposer<TypedFlatConfigItem, ConfigNames> = antfu()
+
+export default config
